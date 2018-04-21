b41acc5802d23216169ce328cba6cc3c065cfbf1
Satyam <satyam7991@gmail.com>
2018-04-21 12:20:40 +0530

    body-parser added

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bp = require('body-parser');
 const app = express();
+
+app.use(bp.json());
+app.use(bp.urlencoded({extended: true}));
 
 app.use('/',express.static(__dirname+"/public_static"));
 
