06d69eef3789758d566bc2c6dc632206b2fae42c
Satyam <satyam7991@gmail.com>
2018-04-30 14:07:30 +0530

    logger created

---
+++ logger.js
@@ -0,0 +1,17 @@
+const winston = require('winston');
+
+const timestampFormat = () => (new Date()).toLocaleTimeString();
+
+const logger = new winston.Logger({
+	transports: [
+		new winston.transports.Console({
+			colorize: true,
+			timestamp: timestampFormat
+		})
+	]
+});
+
+// logger.level = 'debug';
+
+// logger.info("hello winston");
+// logger.debug("debugging info");
